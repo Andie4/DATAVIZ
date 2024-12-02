535d3a62f20e213f2d28d7d1c1bec98b547153a1
Meylie <emilie.desgranges78@gmail.com>
2024-12-02 23:58:16 +0100

    Correction mineur
    > en trop
    script inutile qui cause une erreur
    css inutile et cassé enlevé
    fichier inutile suprimer

--- script/script.js
+++ script/script.js
@@ -1,27 +1,27 @@
 
 
-const timelineContainer = document.createElement("div");
-timelineContainer.className = "timeline";
+// const timelineContainer = document.createElement("div");
+// timelineContainer.className = "timeline";
 
-timelineData.timeline.forEach(item => {
-  const timelineItem = document.createElement("div");
-  timelineItem.className = `timeline-item year-${item.year}`;
+// timelineData.timeline.forEach(item => {
+//   const timelineItem = document.createElement("div");
+//   timelineItem.className = `timeline-item year-${item.year}`;
   
-  const yearElement = document.createElement("p");
-  yearElement.className = "year";
-  yearElement.textContent = item.year;
+//   const yearElement = document.createElement("p");
+//   yearElement.className = "year";
+//   yearElement.textContent = item.year;
   
-  const squareElement = document.createElement("div");
-  squareElement.className = "year-square";
+//   const squareElement = document.createElement("div");
+//   squareElement.className = "year-square";
   
-  const descriptionElement = document.createElement("p");
-  descriptionElement.className = "description";
-  descriptionElement.textContent = item.description;
+//   const descriptionElement = document.createElement("p");
+//   descriptionElement.className = "description";
+//   descriptionElement.textContent = item.description;
   
-  timelineItem.appendChild(yearElement);
-  timelineItem.appendChild(squareElement);
-  timelineItem.appendChild(descriptionElement);
-  timelineContainer.appendChild(timelineItem);
-});
+//   timelineItem.appendChild(yearElement);
+//   timelineItem.appendChild(squareElement);
+//   timelineItem.appendChild(descriptionElement);
+//   timelineContainer.appendChild(timelineItem);
+// });
 
-document.body.appendChild(timelineContainer);
+// document.body.appendChild(timelineContainer);
