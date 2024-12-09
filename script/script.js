680c7cdf9272db078740d99aeafc1fe660953ff0
syrinedrll <158038765+syrinedrll@users.noreply.github.com>
2024-12-09 20:08:13 +0100

    nouveau

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 
+<<<<<<< Updated upstream
 document.querySelector('.accordion').addEventListener('click', function () {
     const panel = document.querySelector('.panel');
     if (panel.style.maxHeight) {
@@ -7,3 +8,46 @@ document.querySelector('.accordion').addEventListener('click', function () {
       panel.style.maxHeight = panel.scrollHeight + "px"; // Ouvre l'accordéon
     }
   });
+=======
+
+// const timelineContainer = document.createElement("div");
+// timelineContainer.className = "timeline";
+
+// timelineData.timeline.forEach(item => {
+//   const timelineItem = document.createElement("div");
+//   timelineItem.className = `timeline-item year-${item.year}`;
+  
+//   const yearElement = document.createElement("p");
+//   yearElement.className = "year";
+//   yearElement.textContent = item.year;
+  
+//   const squareElement = document.createElement("div");
+//   squareElement.className = "year-square";
+  
+//   const descriptionElement = document.createElement("p");
+//   descriptionElement.className = "description";
+//   descriptionElement.textContent = item.description;
+  
+//   timelineItem.appendChild(yearElement);
+//   timelineItem.appendChild(squareElement);
+//   timelineItem.appendChild(descriptionElement);
+//   timelineContainer.appendChild(timelineItem);
+// });
+
+// document.body.appendChild(timelineContainer);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const accordion = document.querySelector(".accordion");
+  const panel = document.querySelector(".panel");
+
+  accordion.addEventListener("click", () => {
+      // Toggle le display
+      if (panel.style.display === "block") {
+          panel.style.display = "none";
+      } else {
+          panel.style.display = "block";
+      }
+  });
+});
+>>>>>>> Stashed changes
