f6f35e5d2331a8224fd7901d33dadb74ee87f28b
Meylie <emilie.desgranges78@gmail.com>
2024-12-11 11:36:31 +0100

    Maj détail
    Plus de commentaire : todo
    Tableau display none
    source graph

--- script/script.js
+++ script/script.js
@@ -3,13 +3,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const accordion = document.querySelector(".accordion");
   const panel = document.querySelector(".panel");
+  panel.style.display="none";
 
   accordion.addEventListener("click", () => {
       // Toggle le display
       if (panel.style.display === "block") {
           panel.style.display = "none";
+          accordion.innerHTML="Afficher les données";
       } else {
           panel.style.display = "block";
+          accordion.innerHTML="Cacher les données";
       }
   });
 });
